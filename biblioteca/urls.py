81752bf25a99d0a8c4a5f9c998b804e1ed8f0fc5
paola6522 <paola.uribe05@inacapmail.cl>
2025-09-28 23:26:01 -0300

    se me olvido la contraseña

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -20,5 +20,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='inicio'), name='logout'), # Cerrar sesión del usuario (usa vista genérica de Django)
     path('registro/', registro_view, name='registro'), # Registro personalizado de usuario
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'), # Inicio de sesión con plantilla personalizada
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('estadisticas/', views.estadisticas, name='estadisticas'), # Página de estadísticas de lectura (gráficos, totales, etc.)
 ]
--- letrasproject/settings.py
+++ letrasproject/settings.py
@@ -134,3 +134,11 @@ LOGIN_REDIRECT_URL = 'inicio'
 LOGOUT_REDIRECT_URL = 'login'
 
 DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tucorreo@gmail.com'
+EMAIL_HOST_PASSWORD = 'tupasswordOAppPassword'
+
