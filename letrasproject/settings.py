bd37e898de42d56503a998a2e85921df84e98300
paola6522 <paola.uribe05@inacapmail.cl>
2025-11-13 19:59:49 -0300

    .

--- letrasproject/settings.py
+++ letrasproject/settings.py
@@ -156,10 +156,16 @@ AUTHENTICATION_BACKENDS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # para archivos estáticos en producción
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
