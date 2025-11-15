1d43b875ba7ea3a16fc058aa7ade6720cfd6712a
paola6522 <paola.uribe05@inacapmail.cl>
2025-11-15 16:45:55 -0300

    settings

--- letrasproject/settings.py
+++ letrasproject/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'letrasproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import dj_database_url
+
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=os.getenv("RENDER", "") != "",
     )
 }
 
