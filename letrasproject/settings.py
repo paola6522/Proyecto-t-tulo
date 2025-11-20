965bbb20e27e6a38480cd06d55d4b33c026fa5c7
paola6522 <paola.uribe05@inacapmail.cl>
2025-11-20 00:25:27 -0300

    .

--- letrasproject/settings.py
+++ letrasproject/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'inicio'
